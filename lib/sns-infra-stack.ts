649935c789b98c1911010db56b1aebf220d6fa0b
brandonmooney1217 <brandonmooney2023@gmail.com>
2025-08-03 22:18:48 -0700

    first commit for creating sns infra

--- lib/sns-infra-stack.ts
+++ lib/sns-infra-stack.ts
@@ -1,16 +1,65 @@
 import * as cdk from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SnsInfraStack extends cdk.Stack {
+  public readonly notificationTopic: sns.Topic;
+  public readonly topicArn: string;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create SNS topic for Spring Boot application notifications
+    this.notificationTopic = new sns.Topic(this, 'NotificationTopic', {
+      displayName: 'Spring Boot Application Notifications',
+      topicName: 'spring-boot-notifications',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'SnsInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.topicArn = this.notificationTopic.topicArn;
+
+    // Create IAM role for Spring Boot application to access SNS
+    const springBootRole = new iam.Role(this, 'SpringBootSnsRole', {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+      roleName: 'SpringBootSnsRole',
+      description: 'IAM role for Spring Boot application to access SNS',
+    });
+
+    // Add SNS permissions for publish, subscribe, and topic management
+    springBootRole.addToPolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: [
+        'sns:Publish',
+        'sns:Subscribe',
+        'sns:Unsubscribe',
+        'sns:CreateTopic',
+        'sns:GetTopicAttributes',
+        'sns:ListTopics',
+        'sns:ListSubscriptionsByTopic',
+      ],
+      resources: [
+        this.notificationTopic.topicArn,
+        `arn:aws:sns:${this.region}:${this.account}:*`,
+      ],
+    }));
+
+    // Export values for Spring Boot application configuration
+    new cdk.CfnOutput(this, 'TopicArn', {
+      value: this.topicArn,
+      description: 'ARN of the SNS topic for Spring Boot application',
+      exportName: 'SpringBootSnsTopicArn',
+    });
+
+    new cdk.CfnOutput(this, 'IAMRoleArn', {
+      value: springBootRole.roleArn,
+      description: 'ARN of the IAM role for Spring Boot application',
+      exportName: 'SpringBootSnsRoleArn',
+    });
+
+    new cdk.CfnOutput(this, 'TopicName', {
+      value: this.notificationTopic.topicName,
+      description: 'Name of the SNS topic',
+      exportName: 'SpringBootSnsTopicName',
+    });
   }
 }
